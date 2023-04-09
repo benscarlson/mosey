41c17e22536a0687622dbd8b9ce4393630503db4
Ben Carlson <benc@Hobbes.local>
2023-04-08 21:51:04 -0400

    initial jekyll website

---
+++ setup.sh
@@ -0,0 +1,30 @@
+setopt interactivecomments
+bindkey -e
+
+cd ~/projects/mosey
+
+rm -rf src
+
+git clone git@github.com:benscarlson/mosey.git src
+
+mv src/docs/* . #Need to remove everything from docs to create jekyll
+
+jekyll new --skip-bundle .
+
+# Edit Gemfile
+
+bundle install
+#https://github.com/jekyll/jekyll/issues/8523
+bundle add webrick
+
+#---- Run jekyll ----
+
+#Seems jekyll serve does all the parsing before serving the web page  
+bundle exec jekyll serve
+
+#rename index.markdown -> index.markdown.old
+#remove the entry from the _posts directory
+#Moved everything out of docs and up a level into src
+#Put assets/images into docs
+
+#Go to http://localhost:4000
